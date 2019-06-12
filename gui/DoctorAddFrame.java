f20dbcd1259d5521ffb48d03b7b0cf6cbe684037
halfershan33 <49019461+halfershan33@users.noreply.github.com>
2019-06-12 11:46:01 -0600

    add a constrouctor for DoctorAddFrame

--- gui/DoctorAddFrame.java
+++ gui/DoctorAddFrame.java
@@ -1,4 +1,26 @@
 package gui;
 
-public class DoctorAddFrame {
+import javax.swing.JFrame;
+
+/**
+ * The frame for the window to enter data for a new patient, and cause the creation of the patient.
+ */
+public class DoctorAddFrame extends JFrame {
+    /** The standard width for the frame. */
+    public static final int DEFAULT_WIDTH = 350;
+
+    /** The standard height for the frame. */
+    public static final int DEFAULT_HEIGHT = 200;
+
+    /**
+     * Create the frame to add a patient.
+     */
+    public DoctorAddFrame() {
+        setTitle("Doctor Addition");
+        setSize(DEFAULT_WIDTH, DEFAULT_HEIGHT);
+        DoctorAddPanel panel = new DoctorAddPanel();
+        add(panel);
+    }
+
+    public static final long serialVersionUID = 1;
 }
