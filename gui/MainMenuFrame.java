23d9c99a262c7ef0b516968193508b7fbdc6f787
halfershan33 <49019461+halfershan33@users.noreply.github.com>
2019-06-14 12:35:06 -0600

    Add panel component and created Main Menu panel

--- gui/MainMenuFrame.java
+++ gui/MainMenuFrame.java
@@ -1,4 +1,22 @@
 package gui;
+import javax.swing.*;
 
-public class MainMenuFrame {
+import startup.GuiHospitalSystem;
+public class MainMenuFrame extends JFrame{
+
+    /** The standard width for the frame. */
+    public static final int DEFAULT_WIDTH = 350;
+
+    /** The standard height for the frame. */
+    public static final int DEFAULT_HEIGHT = 400;
+
+    public MainMenuFrame(){
+    setTitle("Main Menu");
+    setSize(DEFAULT_WIDTH,DEFAULT_HEIGHT);
+    MainMenuPanel panel = new MainMenuPanel();
+    add(panel);
+    }
+
+
+
 }
